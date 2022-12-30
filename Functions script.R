f82fd36049c57308eace837ecddd1be94deaf91f
sdiazuc3m <103251643+sdiazuc3m@users.noreply.github.com>
2022-12-30 12:18:35 +0100

    Update Functions script.R

--- Functions script.R
+++ Functions script.R
@@ -14,8 +14,20 @@ library(truncnorm)    #
 library(gridExtra)    #
 library(cowplot)      #
 library(rjags)        #
-library(asbio)        #
+#library(asbio)        #
 #######################
+
+rinvchisq <- function (n, df, scale = 1/df) 
+{
+  if ((length(scale) != 1) & (length(scale) != n)) 
+    stop("scale should have the same length as n")
+  if (df <= 0) 
+    stop("df must be greater than zero")
+  if (any(scale <= 0)) 
+    stop("scale must be greater than zero")
+  (df * scale)/rchisq(n, df = df)
+}
+
 
 ################################################################################
 
